df480a439ab467b0719d0867f256d90e9e377bc0
Alekxandr <alkv84@yandex.ru>
2017-11-23 23:13:07 +0200

    2

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -1,12 +1,10 @@
 from django import template
-from catalog.models import Category
 
 
 register = template.Library()
 
 
 @register.inclusion_tag('_breadcrumbs.html')
-def business_part_nav(category_id):
-    node = Category.objects.get(id=category_id)
-    pages = node.get_ancestors(ascending=False, include_self=True)
-    return {'pages': pages}
+def breadcrumbs(category, product_title):
+    pages = category.get_ancestors(ascending=False, include_self=True)
+    return {'pages': pages, 'product': product_title}
